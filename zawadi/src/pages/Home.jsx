2243be81a8d6a9bd38d49b3fb7fd1584cc4b4805
achoclate <nyabokeann99@gmail.com>
2024-05-07 03:40:16 -0700

    update product

--- zawadi/src/pages/Home.jsx
+++ zawadi/src/pages/Home.jsx
@@ -1,8 +1,28 @@
 
-import React from 'react';
+import React, {useContext} from 'react';
+import { ProductContext } from '../context/ProductContext';
 
+import Product from '../components/Product';
 const Home = () => {
-  return <div>Homepage</div>;
+    const {products} = useContext(ProductContext)
+    const filteredProducts = products.filter(item=>{
+        return ( item.category === "women's clothing" || item.category ==="electronics" || item.category === "men's clothing"
+    );
+    });
+  return <div>
+    <section className='py-16'>
+        <div className="container mx-auto">
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:gridcols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+                {filteredProducts.map(product =>{
+                    return (
+                        <Product product={product} key={product.id}/>
+                    );
+                }
+                )}
+            </div>
+        </div>
+    </section>
+  </div>;
 };
 
 export default Home;
