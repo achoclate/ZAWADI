41e61132754f900b2df8f3c13f2024a176bb0aa0
achoclate <nyabokeann99@gmail.com>
2024-05-07 03:53:23 -0700

    update

--- zawadi/src/components/Product.jsx
+++ zawadi/src/components/Product.jsx
@@ -17,7 +17,7 @@ const Product = ({ product }) => {
             />
           </div>
         </div>
-        <div className='absolute bottom-6 right-11 group-hover:right-5 bg-red-500/40 p-2 flex flex-row items-center justify-center gap-y-2 opacity-0 group hover:opacity-100 transition-all duration-250'>
+        <div className='absolute bottom-0 right-4 group-hover:right-0 bg-red-500/40 p-1 flex flex-row items-center justify-center gap-y-2 opacity-0 group hover:opacity-100 transition-all duration-250'>
           <button>
             <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500/40'>
               <BsPlus className='text-3xl' />
