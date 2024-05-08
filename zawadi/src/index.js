3569ea13d9440ea5115859dc64c7067e10b67a92
achoclate <nyabokeann99@gmail.com>
2024-05-07 17:32:47 -0700

    mount sidebar on index.js

--- zawadi/src/index.js
+++ zawadi/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ProductProvider from './context/ProductContext';
-
+import Sidebar from './components/Sidebar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Sidebar>
   <ProductProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
   </ProductProvider>
+  </Sidebar>
 );
