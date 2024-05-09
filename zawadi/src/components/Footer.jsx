c67db76a367e6355bb5c8e920f67182e5aac54f1
achoclate <nyabokeann99@gmail.com>
2024-05-09 00:13:24 -0700

    add footer details

--- zawadi/src/components/Footer.jsx
+++ zawadi/src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return <footer className='bg-blue-200 py-6'>
     <div>
-      <p className='text-primary text-center'>copyright &copy; Zawadi 2024. All rights reserved.</p>
+      <p className='text-primary text-center'>A promise of a hassle-free shopping</p>
+      <p className='text-primary text-center italic'>copyright &copy; Zawadi 2024. All rights reserved.</p>
+
       </div>
       </footer>;
 };
--- zawadi/src/components/Sidebar.jsx
+++ zawadi/src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ import { CartContext } from '../context/CartContext';
 
 const Sidebar = () => {
 const { isOpen, handleClose } = useContext(SidebarContext); 
-const {cart, clearCart, total} = useContext(CartContext);
+const {cart, clearCart, total, itemAmount} = useContext(CartContext);
   return (
     <div
       className={`${
@@ -18,7 +18,7 @@ const {cart, clearCart, total} = useContext(CartContext);
       w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw]transition-all duration-250 z-20 px-4 lg:px-[35px]`}>
     
       <div className='flex items-center justify-between py-6 border-b'>
-        <div className='uppercase text-sm font-semibold'>Shopping Cart(0)</div>
+        <div className='uppercase text-sm font-semibold'>Shopping Cart({itemAmount})</div>
 
         <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
         <IoArrowForwardCircle className='text-2xl'/>
