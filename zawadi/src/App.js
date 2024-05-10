25bd4e9e8172c7fa294c654a97dc0ee9ac4e5fa6
achoclate <nyabokeann99@gmail.com>
2024-05-10 00:52:05 -0700

    add the login form

--- zawadi/src/App.js
+++ zawadi/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Shop from './pages/Shop';
-import Contact from './pages/Contact'; // Import the Contact component
+import Contact from './pages/Contact';
 import ProductDetails from './pages/ProductDetails';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Login from './pages/Login'; // Import the Login component
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
           <Route path='/shop' element={<Shop />} />
           <Route path='/contact' element={<Contact />} /> 
           <Route path='/product/:id' element={<ProductDetails />} />
+          <Route path='/login' element={<Login />} /> {/* Add route for Login */}
         </Routes>
         <Sidebar />
         <Footer />
