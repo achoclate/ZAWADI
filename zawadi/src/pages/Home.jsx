5ddbc4e381fc490c6fa25c654b264beff695c2d7
achoclate <nyabokeann99@gmail.com>
2024-05-10 19:12:43 -0700

    update home page

--- zawadi/src/pages/Home.jsx
+++ zawadi/src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
     <div>
-      HOME
+      <video width="100%" height="auto" autoPlay loop muted>
+        <source src={require('../image/video.mp4')} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
