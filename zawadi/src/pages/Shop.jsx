9a4a45a8e1fc72b585891846ca3635354fa7c06a
achoclate <nyabokeann99@gmail.com>
2024-05-11 16:10:40 -0700

    work on the product details page to ensure it is working as expected

--- zawadi/src/pages/Shop.jsx
+++ zawadi/src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom'; // Import Link
 import { ProductContext } from '../context/ProductContext';
 import Product from '../components/Product';
 
@@ -16,14 +17,16 @@ const Shop = () => {
                         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
                             {filteredProducts.map(product => {
                                 return (
-                                    <Product product={product} key={product.id} />
+                                    // Wrap each product in a Link component with appropriate 'to' attribute
+                                    <Link to={`/product/${product.id}`} key={product.id}>
+                                        <Product product={product} />
+                                    </Link>
                                 );
                             })}
                         </div>
                     </div>
                 </section>
             </div>
-           
         </div>
     );
 };
