7903abf2ebc7edb0e3f8ba1a0fe140ad990f23d5
achoclate <nyabokeann99@gmail.com>
2024-05-12 18:14:56 -0700

    add more details on navbar

--- zawadi/src/components/Navbar.jsx
+++ zawadi/src/components/Navbar.jsx
@@ -13,18 +13,16 @@ import { ProductContext } from '../context/ProductContext';
 
 const Navbar = () => {
     const Menus = [
-        { name: 'Home', icon: RiHomeSmileFill, dis: 'translate-x-0', link: '/' },
-        { name: 'Shop', icon: BsShopWindow, dis: 'translate-x-16', link: '/shop' },
-        { name: 'Contact', icon: PiPhoneCallFill, dis: 'translate-x-32', link: '/contact' },
-        { name: 'Login', icon: CgProfile, dis: 'translate-x-48', link: '/login' },
+        { name: 'Home', icon: RiHomeSmileFill, link: '/' },
+        { name: 'Shop', icon: BsShopWindow, link: '/shop' },
+        { name: 'Contact', icon: PiPhoneCallFill, link: '/contact' },
+        { name: 'Login', icon: CgProfile, link: '/login' },
     ];
 
     const { itemAmount } = useContext(CartContext);
     const { setIsOpen } = useContext(SidebarContext);
     const { products } = useContext(ProductContext);
 
-    const [isActive, setIsActive] = useState(false);
-    const [activeIndex, setActiveIndex] = useState(-1);
     const [searchQuery, setSearchQuery] = useState('');
     const [filteredProducts, setFilteredProducts] = useState([]);
     const [showDropdown, setShowDropdown] = useState(false); 
@@ -36,10 +34,6 @@ const Navbar = () => {
             )
         );
     }, [products, searchQuery]);
-
-    const handleMenuClick = (index) => {
-        setActiveIndex(index);
-    };
 
     const handleCartClick = () => {
         setIsOpen(prevState => !prevState);
@@ -64,74 +58,53 @@ const Navbar = () => {
     }, []);
 
     return (
-        <div className="relative">
-            <div className="bg-gray-800 py-4 px-4 md:flex md:items-center md:justify-between">
-                <div className="flex items-center">
-                    <Link to={'/shop'}>
-                        <img className='w-12 md:w-[40px]' src={Logo} alt='' />
-                    </Link>
-                    <div className="ml-3 flex items-center">
-                        <input
-                            type='text'
-                            value={searchQuery}
-                            onChange={handleSearchChange}
-                            placeholder='Search...'
-                            className='px-3 py-1 mr-2 border rounded'
-                        />
-                        <FaSearch className='cursor-pointer' />
-                        {/* Dropdown to show search results */}
-                        {showDropdown && (
-                            <div className="search-dropdown absolute bg-white mt-2 p-2 shadow-md border rounded w-full left-0 top-full"> {/* Adjust top-full to place the dropdown below */}
-                                {filteredProducts.map(product => (
-                                    <div key={product.id}>{product.title}</div>
-                                ))}
-                                {filteredProducts.length === 0 && (
-                                    <div>No products found.</div>
-                                )}
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <div className="mt-4 md:mt-0">
-                    <button onClick={handleCartClick} className='text-white focus:outline-none'>
-                        <TiShoppingCart className='text-2xl' />
-                        <div className='bg-red-300 absolute -left-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>
-                            {itemAmount}
+        <nav className="bg-gray-800 py-4 px-4 md:flex md:items-center md:justify-between">
+            <div className="flex items-center justify-between">
+                <Link to={'/shop'}>
+                    <img className='w-12 md:w-20' src={Logo} alt='' />
+                </Link>
+                <div className="ml-3 flex items-center">
+                    <input
+                        type='text'
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                        placeholder='Search...'
+                        className='px-3 py-1 mr-2 border rounded'
+                    />
+                    <FaSearch className='cursor-pointer' />
+                    {/* Dropdown to show search results */}
+                    {showDropdown && (
+                        <div className="search-dropdown absolute bg-white mt-2 p-2 shadow-md border rounded w-full left-0 top-full">
+                            {filteredProducts.map(product => (
+                                <div key={product.id}>{product.title}</div>
+                            ))}
+                            {filteredProducts.length === 0 && (
+                                <div>No products found.</div>
+                            )}
                         </div>
-                    </button>
-                    <button onClick={() => setIsActive(!isActive)} className='ml-4 md:hidden text-white focus:outline-none'>
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            className='h-6 w-6'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            stroke='currentColor'
-                        >
-                            <path
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                strokeWidth='2'
-                                d={isActive ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}
-                            />
-                        </svg>
-                    </button>
+                    )}
                 </div>
             </div>
-            <div className={`${isActive ? 'block' : 'hidden'} md:hidden bg-gray-800 py-2 px-4`}>
-                {Menus.map((menu, i) => (
-                    <Link
-                        key={i}
-                        to={menu.link}
-                        className={`block py-2 text-white text-center ${
-                            activeIndex === i ? 'bg-pink-600' : ''
-                        }`}
-                        onClick={() => handleMenuClick(i)}
-                    >
-                        {menu.name}
-                    </Link>
-                ))}
+            <div className="mt-4 md:mt-0 flex items-center">
+                <button onClick={handleCartClick} className='text-white focus:outline-none mr-4'>
+                    <TiShoppingCart className='text-2xl' />
+                    <div className='bg-red-300 absolute -left-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>
+                        {itemAmount}
+                    </div>
+                </button>
+                <div className="hidden md:block">
+                    {Menus.map((menu, i) => (
+                        <Link
+                            key={i}
+                            to={menu.link}
+                            className={`text-white mr-4 hover:text-gray-300`}
+                        >
+                            {menu.name}
+                        </Link>
+                    ))}
+                </div>
             </div>
-        </div>
+        </nav>
     );
 };
 
